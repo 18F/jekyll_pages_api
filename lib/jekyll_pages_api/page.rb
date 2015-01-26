a464fb94a7c5d8576c0e47e183d9bf4d3ebe1fa4
Aidan Feldman <aidan.feldman@gsa.gov>
2015-01-26 05:47:49 -0500

    handle the baseurl

--- lib/jekyll_pages_api/page.rb
+++ lib/jekyll_pages_api/page.rb
@@ -21,6 +21,14 @@ module JekyllPagesApi
       self.filterer.decode_html(self.page.data['title'] || '')
     end
 
+    def base_url
+      self.page.site.baseurl
+    end
+
+    def url
+      [self.base_url, self.page.url].join
+    end
+
     def body_text
       self.filterer.text_only(self.page.content)
     end
@@ -28,7 +36,7 @@ module JekyllPagesApi
     def to_json
       {
         title: self.title,
-        url: page.url,
+        url: self.url,
         body: self.body_text
       }
     end
