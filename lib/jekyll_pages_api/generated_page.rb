0ba4ea633e993dbbaf98db6cdd8336391afae334
Mike Bland <michael.bland@gsa.gov>
2015-06-22 17:02:49 -0400

    Add spec/generated_page_spec.rb, fix bugs
    Also tweaked comments and reordered the arguments to GeneratedPage.new.

--- lib/jekyll_pages_api/generated_page.rb
+++ lib/jekyll_pages_api/generated_page.rb
@@ -8,20 +8,25 @@ module JekyllPagesApi
   class GeneratedPage
     attr_reader :path, :relative_path, :data, :content
 
+    # @param path [String] full path to the generated page's file
     # @param basedir see {GeneratedSite#initialize}
     # @param title_prefix see {GeneratedSite#initialize}
     # @param body_element_tag see {GeneratedSite#initialize}
-    # @param path [String] full path to the generated page's file
     # @param content [String] HTML content of the generated page's file
-    def initialize(basedir, title_prefix, body_element_tag, path, content)
+    # @raises [RuntimError] if path does not begin with basedir
+    def initialize(path, basedir, title_prefix, body_element_tag, content)
+      unless path.start_with? basedir
+        raise "#{path} does not start with #{basedir}"
+      end
+
       @path = path
       basedir_len = basedir.size
-      basedir_len += 1 if basedir.end_with? File::SEPARATOR
+      basedir_len -= File::SEPARATOR.size if basedir.end_with? File::SEPARATOR
 
       end_path = path.size
       index_suffix = File.join "", "index.html"
-      end_path -= (index_suffix.size + 1) if path.end_with? index_suffix
-      @relative_path = path[basedir_len..end_path]
+      end_path -= index_suffix.size if path.end_with? index_suffix
+      @relative_path = (path[basedir_len..end_path] || "")
       @data, @content = GeneratedPageParser.parse_generated_page(
         content, title_prefix, body_element_tag)
     end
--- lib/jekyll_pages_api/generated_site.rb
+++ lib/jekyll_pages_api/generated_site.rb
@@ -16,9 +16,9 @@ module JekyllPagesApi
     # @param basedir [String] Path to the generated site's root directory
     # @param title_prefix [String] Prefix to strip from page titles
     # @param body_element_tag [String] Tag (or tag prefix) identifying the
-    #   main content element of each tag. Can be a complete tag (ending in
-    #   '>'), or the prefix of a longer tag. Used to strip boilerplate out of
-    #   the content exported via the API.
+    #   main content element within the <body> element of each document. Can
+    #   be a complete tag (ending in '>'), or the prefix of a longer tag. Used
+    #   to strip boilerplate out of the content exported via the API.
     def initialize(baseurl, basedir, title_prefix, body_element_tag)
       @baseurl = baseurl
       @basedir = basedir
@@ -33,8 +33,8 @@ module JekyllPagesApi
       Dir.glob(File.join(self.basedir, '**', '*')) do |f|
         next unless f.end_with? '.html'
         begin
-          page = GeneratedPage.new(self.basedir, self.title_prefix,
-            self.body_element_tag, f, File.read(f))
+          page = GeneratedPage.new(f, self.basedir, self.title_prefix,
+            self.body_element_tag, File.read(f))
           yield page unless page.data['title'].nil?
         rescue
           $stderr.puts "Error while processing #{f}:"
